9516a7b409666b0e150420b1a775219760fc83fb
Mirnes Kudic <mirnes.kudic@icloud.com>
2025-12-17 14:54:34 -0500

    Add demo user store for JWT authentication (admin:adminpass)

---
+++ js-bridge/users.js
@@ -0,0 +1,5 @@
+// Demo user store – in production use a database
+const users = [
+  { id: 1, username: 'admin', passwordHash: '$2a$10$YXo2Qmj7MUM3WfYZFVbJ2OYAcSzGoUQ3yVeq6OUk2iMTFjqk4yVAW' } // password: adminpass
+];
+module.exports = users;
